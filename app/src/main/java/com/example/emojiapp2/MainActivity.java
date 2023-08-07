026853342e3b470d427ea4d4fbca4eb559e5acc1
gastonlopez5 <43733902+gastonlopez5@users.noreply.github.com>
2023-08-07 09:18:24 -0300

    emoji test

--- app/src/main/java/com/example/emojiapp2/MainActivity.java
+++ app/src/main/java/com/example/emojiapp2/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.emojiapp2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -14,41 +16,41 @@ import com.vanniktech.emoji.EmojiTextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView bt_emoji, bt_send;
+    private Button btn_emoji;
     private EditText et_emoji;
-    private LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bt_emoji = findViewById(R.id.bt_emoji);
-        bt_send = findViewById(R.id.bt_sent);
+        btn_emoji = findViewById(R.id.btn_emoji);
         et_emoji = findViewById(R.id.et_emoji);
-        linearLayout = findViewById(R.id.linear_layout);
 
-        EmojiPopup popup = EmojiPopup.Builder.fromRootView(findViewById(R.id.linear_layout)).build(et_emoji);
-
-        bt_emoji.setOnClickListener(new View.OnClickListener() {
+        btn_emoji.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                LayoutInflater inflater = getLayoutInflater();
+                View view = inflater.inflate(R.layout.layout_emoji, null);
+                builder.setView(view);
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+                EmojiPopup popup = EmojiPopup.Builder.fromRootView(view.findViewById(R.id.root_view)).build(et_emoji);
                 popup.toggle();
-            }
-        });
 
-        bt_send.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                EmojiTextView emojiTextView = (EmojiTextView) LayoutInflater
-                        .from(v.getContext())
-                        .inflate(R.layout.emoji_text_view, linearLayout, false);
-
-                emojiTextView.setText(et_emoji.getText().toString());
-
-                linearLayout.addView(emojiTextView);
-
-                et_emoji.getText().clear();
+//                EmojiTextView emojiTextView = (EmojiTextView) LayoutInflater
+//                        .from(v.getContext())
+//                        .inflate(R.layout.emoji_text_view, R.id.linear_layout, false);
+//
+//                emojiTextView.setText(et_emoji.getText().toString());
+//
+//                linearLayout.addView(emojiTextView);
+//
+//                et_emoji.getText().clear();
             }
         });
     }
